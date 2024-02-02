af5b9e6e64ef0ffea2e1a7d7a46e08482d1194df
rise0chen <rise0chen@163.com>
2024-02-02 15:41:58 +0800

    feat

--- src/config.rs
+++ src/config.rs
@@ -1,29 +1,61 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum StatisticPeriod {
+    M5,
+    H1,
+    H6,
+    H24,
+}
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum StatisticSide {
+    Up,
+    Down,
+}
+#[derive(Clone, Serialize, Deserialize)]
+pub struct StatisticAlerter {
+    pub period: StatisticPeriod,
+    pub side: StatisticSide,
+    pub value: f64,
+    pub times: u32,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum PriceQuote {
+    Native,
+    Usd,
+}
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum PriceSide {
+    Over,
+    Under,
+}
+#[derive(Clone, Serialize, Deserialize)]
+pub struct PriceAlerter {
+    pub quote: PriceQuote,
+    pub side: PriceSide,
+    pub value: f64,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Alerter {
     pub name: String,
     pub chain: String,
     pub pair: String,
-    pub statistics: Statistics,
-    pub price_native: Vec<f64>,
-    pub price_usd: Vec<f64>,
+    pub statistic: Vec<StatisticAlerter>,
+    pub price: Vec<PriceAlerter>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Notifications {
     pub emails: Vec<String>,
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Statistics {
-    pub m5: Vec<f64>,
-    pub h1: Vec<f64>,
-    pub h6: Vec<f64>,
-    pub h24: Vec<f64>,
-}
-
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Config {
     pub alerters: Vec<Alerter>,
     pub notifications: Notifications,
--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,14 @@ const PERIOD: u64 = 5 * 60;
 #[tokio::main]
 async fn main() {
     let config: Config = serde_json::from_reader(std::fs::File::open("config.json").unwrap()).unwrap();
+    let mut alerters = config.alerters.clone();
     let noticer = noticer::Noticer::new(config.notifications);
 
     let dexscreener = dexscreener::Client::new();
     loop {
         let now = clock_source::now() / 1_000_000_000;
 
-        for alerter in &config.alerters {
+        for (alerter_index, alerter) in alerters.iter_mut().enumerate() {
             let mut pairs = if let Ok(pair) = dexscreener.pairs(&alerter.chain, [&alerter.pair]).await {
                 pair.pairs.unwrap_or_default()
             } else {
@@ -24,72 +25,101 @@ async fn main() {
             let Some(pair) = pairs.pop() else {
                 continue;
             };
+            let price_usd = pair.price_usd.as_ref().and_then(|x| x.parse::<f64>().ok()).unwrap_or_default();
+            let price_native = pair.price_native.parse::<f64>().unwrap_or_default();
+            let quote_token = pair.quote_token.symbol;
 
-            if (0..PERIOD).contains(&(now % (5 * 60))) {
-                for alert in &alerter.statistics.m5 {
-                    let price_change = pair.price_change.m5;
-                    if alert < &0.0 && &price_change < alert {
-                        noticer.notice(&format!("{} m5 down: {}%", alerter.name, price_change)).await;
+            for (alert_index, alert) in alerter.statistic.iter_mut().enumerate() {
+                let price_change = match alert.period {
+                    config::StatisticPeriod::M5 => {
+                        if (0..PERIOD).contains(&(now % (5 * 60))) {
+                            pair.price_change.m5
+                        } else {
+                            continue;
+                        }
                     }
-                    if alert > &0.0 && &price_change > alert {
-                        noticer.notice(&format!("{} m5 up: {}%", alerter.name, price_change)).await;
+                    config::StatisticPeriod::H1 => {
+                        if (0..PERIOD).contains(&(now % (60 * 60))) {
+                            pair.price_change.h1
+                        } else {
+                            continue;
+                        }
                     }
-                }
-            }
-            if (0..PERIOD).contains(&(now % (60 * 60))) {
-                for alert in &alerter.statistics.h1 {
-                    let price_change = pair.price_change.h1;
-                    if alert < &0.0 && &price_change < alert {
-                        noticer.notice(&format!("{} h1 down: {}%", alerter.name, price_change)).await;
+                    config::StatisticPeriod::H6 => {
+                        if (0..PERIOD).contains(&(now % (6 * 60 * 60))) {
+                            pair.price_change.h6
+                        } else {
+                            continue;
+                        }
                     }
-                    if alert > &0.0 && &price_change > alert {
-                        noticer.notice(&format!("{} h1 up: {}%", alerter.name, price_change)).await;
+                    config::StatisticPeriod::H24 => {
+                        if (0..PERIOD).contains(&(now % (24 * 60 * 60))) {
+                            pair.price_change.h24
+                        } else {
+                            continue;
+                        }
                     }
-                }
-            }
-            if (0..PERIOD).contains(&(now % (6 * 60 * 60))) {
-                for alert in &alerter.statistics.h6 {
-                    let price_change = pair.price_change.h6;
-                    if alert < &0.0 && &price_change < alert {
-                        noticer.notice(&format!("{} h6 down: {}%", alerter.name, price_change)).await;
+                };
+
+                match alert.side {
+                    config::StatisticSide::Up => {
+                        if price_change > alert.value {
+                            if alert.times <= 1 {
+                                let times = config.alerters[alerter_index].statistic[alert_index].times;
+                                alert.times = times;
+                                let msg = format!(
+                                    "{} up: {}%({}), ${}, {}{}",
+                                    alerter.name, price_change, times, price_usd, price_native, quote_token
+                                );
+                                noticer.notice(&msg).await;
+                            } else {
+                                alert.times -= 1;
+                            }
+                        } else {
+                            alert.times = config.alerters[alerter_index].statistic[alert_index].times;
+                        }
                     }
-                    if alert > &0.0 && &price_change > alert {
-                        noticer.notice(&format!("{} h6 up: {}%", alerter.name, price_change)).await;
-                    }
-                }
-            }
-            if (0..PERIOD).contains(&(now % (24 * 60 * 60))) {
-                for alert in &alerter.statistics.h24 {
-                    let price_change = pair.price_change.h24;
-                    if alert < &0.0 && &price_change < alert {
-                        noticer.notice(&format!("{} h24 down: {}%", alerter.name, price_change)).await;
-                    }
-                    if alert > &0.0 && &price_change > alert {
-                        noticer.notice(&format!("{} h24 up: {}%", alerter.name, price_change)).await;
+                    config::StatisticSide::Down => {
+                        if price_change < -alert.value {
+                            if alert.times <= 1 {
+                                let times = config.alerters[alerter_index].statistic[alert_index].times;
+                                alert.times = times;
+                                let msg = format!(
+                                    "{} down: {}%({}), ${}, {}{}",
+                                    alerter.name, price_change, times, price_usd, price_native, quote_token
+                                );
+                                noticer.notice(&msg).await;
+                            } else {
+                                alert.times -= 1;
+                            }
+                        } else {
+                            alert.times = config.alerters[alerter_index].statistic[alert_index].times;
+                        }
                     }
                 }
             }
 
-            if let Ok(price) = pair.price_native.parse::<f64>() {
+            for alert in alerter.price.iter_mut() {
+                let price = match alert.quote {
+                    config::PriceQuote::Native => price_native,
+                    config::PriceQuote::Usd => price_usd,
+                };
+                if price == 0.0 {
+                    continue;
+                };
                 let price_old = price * (1.0 - pair.price_change.m5 / 100.0);
-                let native = pair.quote_token.symbol;
-                for alert in &alerter.price_native {
-                    if alert < &0.0 && price < alert.abs() && price_old > alert.abs() {
-                        noticer.notice(&format!("{} under: {}{}", alerter.name, price, native)).await;
+                match alert.side {
+                    config::PriceSide::Over => {
+                        if price > alert.value && price_old < alert.value {
+                            let msg = format!("{} over: ${}, {}{}", alerter.name, price_usd, price_native, quote_token);
+                            noticer.notice(&msg).await;
+                        }
                     }
-                    if alert > &0.0 && price > *alert && price_old < *alert {
-                        noticer.notice(&format!("{} over: {}{}", alerter.name, price, native)).await;
-                    }
-                }
-            }
-            if let Some(price) = pair.price_usd.and_then(|x| x.parse::<f64>().ok()) {
-                let price_old = price * (1.0 - pair.price_change.m5 / 100.0);
-                for alert in &alerter.price_usd {
-                    if alert < &0.0 && price < alert.abs() && price_old > alert.abs() {
-                        noticer.notice(&format!("{} under: ${}", alerter.name, price)).await;
-                    }
-                    if alert > &0.0 && price > *alert && price_old < *alert {
-                        noticer.notice(&format!("{} over: ${}", alerter.name, price)).await;
+                    config::PriceSide::Under => {
+                        if price < alert.value && price_old > alert.value {
+                            let msg = format!("{} under: ${}, {}{}", alerter.name, price_usd, price_native, quote_token);
+                            noticer.notice(&msg).await;
+                        }
                     }
                 }
             }
